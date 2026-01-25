7022579666d3768d726e1a6331b91aad59b03b70
Bira Lavor <umeneses@student.42sp.org.br>
2026-01-25 10:56:48 -0300

    chore: improving main readability

--- Module07/ex02/src/main.cpp
+++ Module07/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: umeneses <umeneses@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 19:49:48 by umeneses          #+#    #+#             */
-/*   Updated: 2026/01/25 10:37:26 by umeneses         ###   ########.fr       */
+/*   Updated: 2026/01/25 10:55:25 by umeneses         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <iostream>
 
 int main(void) {
-    std::cout << "Creating an Array of integers with 5 elements." << std::endl;
+    std::cout << ":::::::: Creating an Array of integers with 5 elements. :::::::" << std::endl;
     Array<int> arr(5);
     for (unsigned int i = 0; i < arr.size(); i++) {
         arr[i] = i * 10;
@@ -24,26 +24,26 @@ int main(void) {
         std::cout << "arr[" << idx << "] = " << arr[idx] << std::endl;
     }
 
-    std::cout << "Creating a copy of the Array using the copy constructor." << std::endl;
+    std::cout << "\n ::::::: Creating a copy of the Array using the copy constructor. :::::::" << std::endl;
     Array<int> copy(arr);
     std::cout << "Copy contents:" << std::endl;
     for (unsigned int idx = 0; idx < copy.size(); idx++) {
         std::cout << "copy[" << idx << "] = " << copy[idx] << std::endl;
     }
 
-    std::cout << "Modifying the original array." << std::endl;
+    std::cout << "\n ::::::: Modifying the original array. :::::::" << std::endl;
     arr[0] = 999;
     std::cout << "Original arr[0]: " << arr[0] << std::endl;
     std::cout << "Copy copy[0]: " << copy[0] << std::endl;
 
-    std::cout << "Assigning the original array to the copy using the assignment operator." << std::endl;
+    std::cout << "\n ::::::: Assigning the original array to the copy using the assignment operator. :::::::" << std::endl;
     copy = arr;
     std::cout << "Copy contents after assignment:" << std::endl;
     for (unsigned int idx = 0; idx < copy.size(); idx++) {
         std::cout << "copy[" << idx << "] = " << copy[idx] << std::endl;
     }
 
-    std::cout << "Attempting to access out-of-bounds index." << std::endl;
+    std::cout << "\n ::::::: Attempting to access out-of-bounds index. :::::::" << std::endl;
     try {
         std::cout << "Accessing arr[10]...." << arr[10] << std::endl;
     } catch (const std::exception &e) {
